02952e42c55a3ea0bfbeea39a086cf1aa9f0f0cb
Nozomu Hatta <nozomuhatta@gmail.com>
2021-05-29 00:02:03 +0900

    Add -v option to display version
    Currently, display git commit hash

--- client/app/main.cpp
+++ client/app/main.cpp
@@ -4,18 +4,26 @@
 #include <iostream>
 #include <string>
 
+#include "version.h"
+
 std::string appname;
 
 void Usage(void) { std::cout << "Usage: " << appname << std::endl; }
+void Version(void) {
+    std::cout << appname << " " << BUILD_VERSION << std::endl;
+}
 
 int main(int argc, char* argv[]) {
     appname = argv[0];
 
     int opt;
-    while ((opt = getopt(argc, argv, "h")) != -1) {
+    while ((opt = getopt(argc, argv, "hv")) != -1) {
         switch (opt) {
             case 'h':
                 Usage();
+                std::exit(EXIT_SUCCESS);
+            case 'v':
+                Version();
                 std::exit(EXIT_SUCCESS);
             default: /* '?' */
                 Usage();
