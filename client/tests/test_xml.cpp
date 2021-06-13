77f677bd1bd971d02dee25b08476ca345c59cc00
Nozomu Hatta <nozomuhatta@gmail.com>
2021-06-13 21:51:07 +0900

    Add search_trkseg_node function

---
+++ client/tests/test_xml.cpp
@@ -0,0 +1,37 @@
+#include <catch2/catch.hpp>
+#include <sstream>
+
+#include "plot/lib.hpp"
+#include "plot/xml.hpp"
+#include "rapidxml.hpp"
+
+TEST_CASE("Parse from entire format", "[gps]") {
+    std::istringstream s1(
+        u8R"##(
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<gpx xmlns="http://www.topografix.com/GPX/1/1" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:mytracks="http://mytracks.stichling.info/myTracksGPX/1/0" creator="myTracks" version="1.1" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd">
+    <trk>
+        <name>track-7</name>
+        <desc>Log every 5 sec, 0 m</desc>
+        <trkseg>
+            <trkpt lat="35.676502228" lon="139.756057739">
+                <ele>17.185059</ele>
+                <time>2012-08-15T03:07:12Z</time>
+                <desc>Lat.=35.676502, Long.=139.756058, Alt.=17.185059m, Speed=0.000000Km/h</desc>
+                <extensions>
+                    <mytracks:speed>0</mytracks:speed>
+                    <mytracks:length>0</mytracks:length>
+                </extensions>
+            </trkpt>
+        </trkseg>
+    </trk>
+</gpx>
+)##");
+
+    rapidxml::xml_node<> *trkseg = search_trkseg_node(&s1);
+    REQUIRE(trkseg != NULL);
+    GpsDataSeg seg;
+    seg.ParseNode(trkseg);
+
+    // REQIRE(seg.size() == 1);
+}
